b2e7ae4c9a59ed9e51de9e28fabe625a6ee5d04c
FariZaOni <vicentefariasaguayo@gmail.com>
2023-05-27 22:15:02 -0400

    identify

--- src/Login.js
+++ src/Login.js
@@ -33,7 +33,7 @@ const Login = () => {
                     withCredentials: true
                 }
             );
-            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response?.data));
             //console.log(JSON.stringify(response));
             const accessToken = response?.data?.accessToken;
             const roles = response?.data?.roles;
